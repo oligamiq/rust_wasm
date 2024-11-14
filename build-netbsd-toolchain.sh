27b04cad391dc66025c2a0c6781f4542fcf1e76f
oligami <nziq53@gmail.com>
2024-11-14 07:31:13 +0000

    patch

--- build-netbsd-toolchain.sh
+++ build-netbsd-toolchain.sh
@@ -97,7 +97,7 @@ cd usr/src
 # * disable various unneeded stuff
 MKUNPRIVED=yes TOOLDIR=/x-tools/x86_64-unknown-netbsd \
 MKSHARE=no MKDOC=no MKHTML=no MKINFO=no MKKMOD=no MKLINT=no MKMAN=no MKNLS=no MKPROFILE=no \
-hide_output ./build.sh -j10 -m amd64 tools
+hide_output ./build.sh -j`nproc` -m amd64 tools
 
 cd ../..
 
