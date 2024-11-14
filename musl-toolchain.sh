f8539cdfabdddea09571b358a8632f4a9f21420e
oligami <nziq53@gmail.com>
2024-11-14 04:53:22 +0000

    update CI workflow to adjust musl toolchain installation and permissions

--- musl-toolchain.sh
+++ musl-toolchain.sh
@@ -30,7 +30,7 @@ exit 1
 }
 
 ARCH=$1
-TARGET=$ARCH-linux-musl
+TARGET=$2
 
 # Don't depend on the mirrors of sabotage linux that musl-cross-make uses.
 LINUX_HEADERS_SITE=https://ci-mirrors.rust-lang.org/rustc/sabotage-linux-tarballs
