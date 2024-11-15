5d0a1ba79a59745d7c206ef2fcb15cb31feff8fc
oligami <nziq53@gmail.com>
2024-11-15 09:35:52 +0000

    Enhance CI workflow with conditional job handling and improve musl-root configuration for Linux targets

--- config_rewriter/src/main.rs
+++ config_rewriter/src/main.rs
@@ -163,25 +163,34 @@ fn main() -> anyhow::Result<()> {
                     OS::False => unreachable!(),
                 }
                 .to_item();
-                doc["target"]["aarch64-unknown-linux-musl"]["musl-root"] =
-                    "/musl-aarch64/aarch64-linux-musl".into();
-                doc["target"]["loongarch64-unknown-linux-musl"]["musl-root"] = "/x-tools/loongarch64-unknown-linux-musl/loongarch64-unknown-linux-musl/sysroot/usr".into();
-                doc["target"]["riscv64gc-unknown-linux-musl"]["musl-root"] =
-                    "/musl-riscv64gc".into();
-                doc["target"]["x86_64-unknown-linux-musl"]["musl-root"] =
-                    "/musl-x86_64/x86_64-linux-musl".into();
-                doc["target"]["arm-unknown-linux-musleabi"]["musl-root"] =
-                    "/musl-arm/arm-linux-musleabi".into();
-                doc["target"]["arm-unknown-linux-musleabihf"]["musl-root"] =
-                    "/musl-armhf/arm-linux-musleabihf".into();
-                doc["target"]["armv5te-unknown-linux-musleabi"]["musl-root"] =
-                    "/musl-armv5te".into();
-                doc["target"]["armv7-unknown-linux-musleabi"]["musl-root"] = "/musl-armv7".into();
-                doc["target"]["armv7-unknown-linux-musleabihf"]["musl-root"] =
-                    "/musl-armv7hf".into();
-                doc["target"]["i586-unknown-linux-musl"]["musl-root"] = "/musl-i586".into();
-                doc["target"]["i686-unknown-linux-musl"]["musl-root"] =
-                    "/musl-i686/i686-linux-musl".into();
+                match os {
+                    OS::Linux => {
+                        doc["target"]["aarch64-unknown-linux-musl"]["musl-root"] =
+                            "/musl-aarch64/aarch64-linux-musl".into();
+                        doc["target"]["loongarch64-unknown-linux-musl"]["musl-root"] = "/x-tools/loongarch64-unknown-linux-musl/loongarch64-unknown-linux-musl/sysroot/usr".into();
+                        doc["target"]["riscv64gc-unknown-linux-musl"]["musl-root"] =
+                            "/musl-riscv64gc".into();
+                        doc["target"]["x86_64-unknown-linux-musl"]["musl-root"] =
+                            "/musl-x86_64/x86_64-linux-musl".into();
+                        doc["target"]["arm-unknown-linux-musleabi"]["musl-root"] =
+                            "/musl-arm/arm-linux-musleabi".into();
+                        doc["target"]["arm-unknown-linux-musleabihf"]["musl-root"] =
+                            "/musl-armhf/arm-linux-musleabihf".into();
+                        doc["target"]["armv5te-unknown-linux-musleabi"]["musl-root"] =
+                            "/musl-armv5te".into();
+                        doc["target"]["armv7-unknown-linux-musleabi"]["musl-root"] =
+                            "/musl-armv7".into();
+                        doc["target"]["armv7-unknown-linux-musleabihf"]["musl-root"] =
+                            "/musl-armv7hf".into();
+                        doc["target"]["i586-unknown-linux-musl"]["musl-root"] = "/musl-i586".into();
+                        doc["target"]["i686-unknown-linux-musl"]["musl-root"] =
+                            "/musl-i686/i686-linux-musl".into();
+                        doc["target"]["wasm32-wasip2"] = doc["target"]["wasm32-wasip1"].clone();
+                    }
+                    OS::Windows => todo!(),
+                    OS::Mac => todo!(),
+                    OS::False => todo!(),
+                }
             }
         }
         Targets::WasmAndX64Linux => {
